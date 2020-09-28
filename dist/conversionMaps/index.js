15db4ca5e24485883143c863bab539067f561f60
dmitriev_sergey <info@davra.cz>
2020-09-28 22:40:18 +0200

    Revert "feature/added-typesctipt"
    This reverts commit b4f301e3882a7b555d313754d5cf5c20bb0409b4.

--- dist/conversionMaps/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const tigerTemplate = require('./TigerTemplate');
-const partnersTemplates = {
-    partnerXy: tigerTemplate
-};
-module.exports = partnersTemplates;
-//# sourceMappingURL=index.js.map
--- dist/schemas/mongodb/partnerCredential.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const { Schema, model } = require('mongoose');
-const schema = new Schema({
-    inbound: { required: true, type: String },
-    outbound: { required: true, type: String },
-    name: { required: true, type: String }
-});
-module.exports = model('partner_credentials', schema);
-//# sourceMappingURL=partnerCredential.js.map
